ba8acf9630e32e041209c8f02ab7ce4c0501c93f
Kivashan <58886204+Kivashan@users.noreply.github.com>
2024-02-26 13:06:24 +0200

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-# installing flask version 2.1.0 using puppet and from pip3
-
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# Using the exec resource to to send a SIGTERM signal to stop a process
-
-exec { 'pkill':
-  command  => 'pkill -15 killmenow',
-  provider => 'shell',
-}
