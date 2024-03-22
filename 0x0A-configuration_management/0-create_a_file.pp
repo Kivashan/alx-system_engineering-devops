ebfa88a03035173ac6a97c152e8962412840097f
Kivashan <bvkivashan1982@gmail.com>
2024-03-22 07:57:21 -0700

    Ammended task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #Puppet file for creating a file
 
-file {'./school':
+file {'/tmp/school':
   ensure  => 'present',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
