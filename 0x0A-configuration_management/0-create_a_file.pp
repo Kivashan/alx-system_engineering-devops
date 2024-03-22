68b7cbfe7a19289fe704705fe9233bd5fd61ba62
Kivashan <bvkivashan1982@gmail.com>
2024-03-22 07:50:45 -0700

    First commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#Puppet file for creating a file
+
+file {'./school':
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
